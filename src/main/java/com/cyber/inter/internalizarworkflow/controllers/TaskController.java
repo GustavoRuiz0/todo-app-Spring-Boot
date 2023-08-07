f2aef2473ef64556e2d054f8113d5e9fb593aac7
cyber <cyber@tux.bbrouter>
2023-08-07 09:57:04 -0400

    corrigindo consumo

--- src/main/java/com/cyber/inter/internalizarworkflow/controllers/TaskController.java
+++ src/main/java/com/cyber/inter/internalizarworkflow/controllers/TaskController.java
@@ -14,6 +14,7 @@ import com.cyber.inter.internalizarworkflow.services.TaskService;
 
 import jakarta.validation.Valid;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
+@CrossOrigin(origins = "*")
 public class TaskController {
     @Autowired
     TaskRepository taskRepository;
